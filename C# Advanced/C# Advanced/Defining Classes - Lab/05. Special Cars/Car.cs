6e7f024f85eac3a794155e56baef21edfe890d98
Petar Milev <37210187+PetarMilev01@users.noreply.github.com>
2022-11-14 20:57:04 +0200

    Add files via upload

---
+++ C# Advanced/C# Advanced/Defining Classes - Lab/05. Special Cars/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarManufacturer;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+        public Engine Engine { get; set; }
+        public Tire[] Tires { get; set; }
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+        public Car(string make, string model, int year) : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption,Engine engine,Tire[] tires)
+            : this(make, model, year,fuelQuantity,fuelConsumption)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+            Engine = engine;
+            Tires = tires;
+        }
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - ((distance * FuelConsumption)/100) >= 0)
+            {
+                FuelQuantity -= (distance * FuelConsumption)/100;
+            }
+            else
+            {
+                Console.WriteLine($"Not enough fuel to perform this trip!");
+            }
+        }
+    }
+}
