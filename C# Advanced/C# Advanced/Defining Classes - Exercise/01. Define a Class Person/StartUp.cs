1545b47e77e1f436c40049a492307e59ad8a6a07
Petar Milev <37210187+PetarMilev01@users.noreply.github.com>
2022-11-15 18:58:44 +0200

    Create StartUp.cs

---
+++ C# Advanced/C# Advanced/Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Person firstPerson = new Person("Peter", 20);
+            Person secondPerson = new Person("George", 18);
+            Person thirdPerson = new Person("Jose", 43);
+        }
+    }
+}
