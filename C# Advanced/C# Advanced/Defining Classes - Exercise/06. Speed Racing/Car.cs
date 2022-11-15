e44b6907d192daafbe6584db12c7174c5b0a050b
Petar Milev <37210187+PetarMilev01@users.noreply.github.com>
2022-11-15 21:19:01 +0200

    Add files via upload

---
+++ C# Advanced/C# Advanced/Defining Classes - Exercise/06. Speed Racing/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKilometer { get; set; }
+        public double TravelledDistance { get; set; }
+
+        
+        public bool Drive(int distance)
+        {
+            if (distance * FuelConsumptionPerKilometer <= FuelAmount)
+            {
+                FuelAmount -= distance * FuelConsumptionPerKilometer;
+                TravelledDistance += distance;
+                return true;
+            }
+
+            return false;
+        }
+
+        public Car(string model, double fuelAmount, double fuelConsumptionFor1Km)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionFor1Km;
+            TravelledDistance = 0.0;
+        }
+
+    }
+}
