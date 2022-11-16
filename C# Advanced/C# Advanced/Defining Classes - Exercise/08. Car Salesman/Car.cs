7927eebb39e0be32e32ffa44cdda4f3348e7e092
Petar Milev <37210187+PetarMilev01@users.noreply.github.com>
2022-11-16 21:19:40 +0200

    Add files via upload

---
+++ C# Advanced/C# Advanced/Defining Classes - Exercise/08. Car Salesman/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public int Weight { get; set; } // optional
+        public string Color { get; set; } //optional
+
+        public Car(string model, Engine engine, int weight, string color)
+        {
+            Model = model;
+            Engine = engine;
+            Weight = weight;
+            Color = color;
+        }
+    }
+}
