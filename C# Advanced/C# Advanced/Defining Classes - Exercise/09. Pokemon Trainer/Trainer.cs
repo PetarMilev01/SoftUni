eb2529db58cbe497a35f785f40c035419cb71311
Petar Milev <37210187+PetarMilev01@users.noreply.github.com>
2022-11-17 17:57:47 +0200

    Add files via upload

---
+++ C# Advanced/C# Advanced/Defining Classes - Exercise/09. Pokemon Trainer/Trainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Trainer
+    {
+        public string Name { get; set; }
+        public int BadgesCount { get;set; }
+        public List<Pokemon> Pokemons { get; set; }
+
+        public Trainer(string name)
+        {
+            Name = name;
+            BadgesCount = 0;
+            Pokemons = new List<Pokemon>();
+        }
+
+    }
+}
