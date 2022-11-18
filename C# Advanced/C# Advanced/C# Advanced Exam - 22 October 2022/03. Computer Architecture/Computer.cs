ac9e38a39685f2d957df377a3296e694a16d67ad
Petar Milev <37210187+PetarMilev01@users.noreply.github.com>
2022-11-18 21:41:10 +0200

    Add files via upload

---
+++ C# Advanced/C# Advanced/C# Advanced Exam - 22 October 2022/03. Computer Architecture/Computer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class Computer
+    {
+        public string Model { get; set; }
+        public int Capacity { get; set; }
+        public List<CPU> Multiprocessor { get; set; }
+
+        public Computer(string model,int capacity)
+        {
+            Model = model;
+            Capacity = capacity;
+            Multiprocessor = new List<CPU>();
+        }
+        public int Count => Multiprocessor.Count;
+
+        public void Add(CPU cpu)
+        {
+            if (Multiprocessor.Count + 1 <= Capacity) 
+            {
+                Multiprocessor.Add(cpu);
+            }
+        }
+
+        public bool Remove(string brand)
+        {
+            if (Multiprocessor.Any(x=>x.Brand == brand))
+            {
+                CPU wanted = Multiprocessor.First(x => x.Brand == brand);
+                Multiprocessor.Remove(wanted);
+                return true;
+            }
+            return false; ;
+        }
+
+        public CPU MostPowerful()
+        {
+            CPU cpu = new CPU("",1,1);
+            foreach (var current in Multiprocessor.OrderByDescending(x=>x.Frequency))
+            {
+                cpu = current;
+                break;
+            }
+            return cpu;
+        }
+
+        public CPU GetCPU(string brand)
+        {
+            return Multiprocessor.FirstOrDefault(x => x.Brand == brand);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"CPUs in the Computer {Model}:");
+           
+            for (int i = 0; i < Multiprocessor.Count; i++)
+            {
+                if (i+1 == Multiprocessor.Count)
+                {
+                    sb.Append(Multiprocessor[i].ToString());
+                }
+                else
+                {
+                    sb.AppendLine(Multiprocessor[i].ToString());
+                }
+               
+            }
+            return sb.ToString();
+        }
+    }
+}
