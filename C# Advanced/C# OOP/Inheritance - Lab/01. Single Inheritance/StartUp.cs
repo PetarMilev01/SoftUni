d654580c34b5c77712371b8d67ef2694aff89591
Petar Milev <37210187+PetarMilev01@users.noreply.github.com>
2022-11-19 14:03:45 +0200

    Create StartUp.cs

---
+++ C# Advanced/C# OOP/Inheritance - Lab/01. Single Inheritance/StartUp.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Bark();
+        }
+    }
+}
