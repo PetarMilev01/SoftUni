c6d265b183c6d908f18274a73086139947301245
Petar Milev <37210187+PetarMilev01@users.noreply.github.com>
2022-11-19 14:53:07 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/Inheritance - Lab/02. Multiple Inheritance/Puppy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Puppy:Dog
+    {
+        public Puppy()
+        {
+
+        }
+        public void Weep()
+        {
+            Console.WriteLine($"weeping…");
+        }
+    }
+}
