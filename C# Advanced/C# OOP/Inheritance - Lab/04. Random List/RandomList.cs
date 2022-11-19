da7301ea051c7d19891c5743019853da835bb301
Petar Milev <37210187+PetarMilev01@users.noreply.github.com>
2022-11-19 15:08:50 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/Inheritance - Lab/04. Random List/RandomList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CustomRandomList
+{
+    public class RandomList:List<string>
+    {
+        public List<string> randomList { get; set; }
+        public RandomList()
+        {
+            randomList = new List<string>();
+        }
+
+        public string RandomString()
+        {
+            Random random = new Random();
+            int element = random.Next(0, randomList.Count);
+            randomList.Remove(randomList[element]);
+            return randomList[element].ToString();
+           
+        }
+    }
+}
