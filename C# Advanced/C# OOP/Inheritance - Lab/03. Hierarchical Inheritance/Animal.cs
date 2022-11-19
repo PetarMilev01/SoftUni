124de8c53396e6e964ac1c8cbbd254fde9e18b2e
Petar Milev <37210187+PetarMilev01@users.noreply.github.com>
2022-11-19 14:54:20 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/Inheritance - Lab/03. Hierarchical Inheritance/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Animal
+    {
+        public Animal()
+        {
+
+        }
+        public void Eat()
+        {
+            Console.WriteLine("eating…");
+        }
+    }
+}
