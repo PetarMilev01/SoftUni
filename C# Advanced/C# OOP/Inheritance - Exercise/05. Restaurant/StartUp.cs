f32e7146cfd2c5ffd5d098d1adf9b61997488d60
Petar Milev <37210187+PetarMilev01@users.noreply.github.com>
2022-11-29 14:14:18 +0200

    Create StartUp.cs

---
+++ C# Advanced/C# OOP/Inheritance - Exercise/05. Restaurant/StartUp.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Globalization;
+using System.Numerics;
+using System.Threading;
+
+namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+           
+        }
+    }
+}
